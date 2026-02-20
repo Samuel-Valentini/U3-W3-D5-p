765a7c667f1f8928815028c7b6c1bd4da6970e03
Samuel-Valentini <samuelvitalia@gmail.com>
2026-02-20 12:03:32 +0100

    Navbar ok

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,15 @@
 import { Col, Container, Row } from "react-bootstrap";
+import NavbarM from "./NavbarM";
 
 const Home = () => {
     return (
         <Container fluid className="w-100 text-white m-0 p-0">
             <Row className="flex-column flex-md-row w-100 m-0 p-0">
-                <Col className="bg-info col-md-2">NavBar</Col>
+                <Col
+                    className="col-md-2 text-white"
+                    style={{ backgroundColor: "#252526" }}>
+                    <NavbarM />
+                </Col>
                 <Col className="bg-warning">Main</Col>
             </Row>
         </Container>
